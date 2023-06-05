b344fb0e873a9a95bb5096a92e732590b9980f60
Fabian Machicao <fabian.machicao@gmail.com>
2023-06-05 18:58:33 -0400

    Premium y tests

--- src/pages/PremiumPage.jsx
+++ src/pages/PremiumPage.jsx
@@ -1,7 +1,41 @@
+import React from 'react';
+import './PremiumPage.css';
+
 const PremiumPage = () => {
-    return(
-        <h1>PremiumPage</h1>
-    )
-}
+  return (
+    <div className="premium-page">
+      <div className="premium-box">
+        <h1>Cambia a cuenta Premium</h1>
+        <div className="feature-box blue-box">
+          <h2>Subtítulo 1</h2>
+          <ul>
+            <li>Característica 1</li>
+            <li>Característica 2</li>
+            <li>Característica 3</li>
+          </ul>
+          <button>Botón 1</button>
+        </div>
+        <div className="feature-box green-box">
+          <h2>Subtítulo 2</h2>
+          <ul>
+            <li>Característica 1</li>
+            <li>Característica 2</li>
+            <li>Característica 3</li>
+          </ul>
+          <button>Botón 2</button>
+        </div>
+        <div className="feature-box light-blue-box">
+          <h2>Subtítulo 3</h2>
+          <ul>
+            <li>Característica 1</li>
+            <li>Característica 2</li>
+            <li>Característica 3</li>
+          </ul>
+          <button>Botón 3</button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default PremiumPage;
--- src/pages/TestPage.jsx
+++ src/pages/TestPage.jsx
@@ -1,7 +1,72 @@
+import React from 'react';
+import './TestPage.css';
+
 const TestPage = () => {
-    return(
-        <h1>TestPage"</h1>
-    )
-}
+  return (
+    <div className="test-page">
+      <div className="header">
+        <div className="title-container">
+          <h1 className="title">Tests de aptitud</h1>
+        </div>
+      </div>
+      <div className="grid">
+        <div className="grid-item active">
+          <div className="icon-container">
+            <img className="icon" src="src/assets/document.png" alt="Document Icon" />
+          </div>
+          <h2 className="test-title">Test 1</h2>
+        </div>
+        <div className="grid-item locked">
+          <div className="icon-container">
+            <img className="icon" src="src/assets/document.png" alt="Document Icon" />
+          </div>
+          <h2 className="test-title">Test 2</h2>
+        </div>
+        <div className="grid-item locked">
+          <div className="icon-container">
+            <img className="icon" src="src/assets/document.png" alt="Document Icon" />
+          </div>
+          <h2 className="test-title">Test 3</h2>
+        </div>
+        <div className="grid-item locked">
+          <div className="icon-container">
+            <img className="icon" src="src/assets/document.png" alt="Document Icon" />
+          </div>
+          <h2 className="test-title">Test 4</h2>
+        </div>
+        <div className="grid-item locked">
+          <div className="icon-container">
+            <img className="icon" src="src/assets/document.png" alt="Document Icon" />
+          </div>
+          <h2 className="test-title">Test 5</h2>
+        </div>
+        <div className="grid-item locked">
+          <div className="icon-container">
+            <img className="icon" src="src/assets/document.png" alt="Document Icon" />
+          </div>
+          <h2 className="test-title">Test 6</h2>
+        </div>
+        <div className="grid-item locked">
+          <div className="icon-container">
+            <img className="icon" src="src/assets/document.png" alt="Document Icon" />
+          </div>
+          <h2 className="test-title">Test 7</h2>
+        </div>
+        <div className="grid-item locked">
+          <div className="icon-container">
+            <img className="icon" src="src/assets/document.png" alt="Document Icon" />
+          </div>
+          <h2 className="test-title">Test 8</h2>
+        </div>
+        <div className="grid-item locked">
+          <div className="icon-container">
+            <img className="icon" src="src/assets/document.png" alt="Document Icon" />
+          </div>
+          <h2 className="test-title">Test 9</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default TestPage;
