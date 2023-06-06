08d9686868e350fdaae3bb13af08142aa029329e
Fabian Machicao <fabian.machicao@gmail.com>
2023-06-06 16:57:35 -0400

    redirección a results

--- guiame/src/pages/PaginaAptitudes.jsx
+++ guiame/src/pages/PaginaAptitudes.jsx
@@ -1,40 +1,42 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import PreguntaLikert from '../components/PreguntaLikert';
 import datos from '../data/test1.json';
 import './PaginaAptitudes.css';
 
 const PaginaAptitudes = () => {
     const [respuestas, setRespuestas] = useState({});
-  
     const handleRespuestaSeleccionada = (pregunta, respuesta) => {
-      setRespuestas((prevRespuestas) => ({
-        ...prevRespuestas,
-        [pregunta]: respuesta,
-      }));
+        setRespuestas((prevRespuestas) => ({
+            ...prevRespuestas,
+            [pregunta]: respuesta,
+        }));
     };
-  
+
     const handleFormSubmit = (e) => {
-      e.preventDefault();
-      // Aquí puedes implementar la lógica para calcular la recomendación de carrera
-      console.log(respuestas);
+        e.preventDefault();
+        // Aquí puedes implementar la lógica para calcular la recomendación de carrera;
+        console.log(respuestas);
     };
-  
+
     return (
-      <div className="container">
-        <h1 className="titulo">Test de aptitudes para carreras universitarias</h1>
-        <form onSubmit={handleFormSubmit}>
-          {datos.map((pregunta, index) => (
-            <PreguntaLikert
-              key={index}
-              pregunta={pregunta.pregunta}
-              opciones={pregunta.opciones}
-              onRespuestaSeleccionada={handleRespuestaSeleccionada}
-            />
-          ))}
-          <button type="submit" className="boton">Obtener recomendación</button>
-        </form>
-      </div>
+        <div className="container">
+            <h1 className="titulo">Test de aptitudes para carreras universitarias</h1>
+            <form onSubmit={handleFormSubmit}>
+                {datos.map((pregunta, index) => (
+                    <PreguntaLikert
+                        key={index}
+                        pregunta={pregunta.pregunta}
+                        opciones={pregunta.opciones}
+                        onRespuestaSeleccionada={handleRespuestaSeleccionada}
+                    />
+                ))}
+                <Link to="/results">
+                    <button type="submit" className="boton">Obtener recomendación</button>
+                </Link>
+            </form>
+        </div>
     );
-  };
-  
-  export default PaginaAptitudes;
+};
+
+export default PaginaAptitudes;
